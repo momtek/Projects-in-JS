5c1a09ae7e863567a65b8decb59899fe551794ff
momtek <momtek.barry@gmail.com>
2018-11-09 13:42:37 +0100

    Updated main.js

--- 06 Analogue Clock/main.js
+++ 06 Analogue Clock/main.js
@@ -1,13 +1,31 @@
 let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2');
+let ctx = canvas.getContext('2d');
 let radius = canvas.height / 2;
-context.translate(radius, radius);
+ctx.translate(radius, radius);
 radius = radius * 0.9;
 
 function drawClock() {
-  context.arc(0, 0, radius, 0, 2 * Math.PI);
-  context.fillStyle = 'White';
-  context.fill();
+  drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  let grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = '#fff';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, '#fff');
+  grad.addColorStop(1, '#333');
+
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius * 0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
 }
 
 drawClock();
